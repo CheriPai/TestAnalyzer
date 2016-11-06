6a4046aafe43930c202e2f18a55b1cd8517d95f9
dat <datdo@rocketmail.com>
2016-11-06 10:34:00 -0800

    Fix regex to match generics

--- testanalyzer/javaanalyzer.py
+++ testanalyzer/javaanalyzer.py
@@ -5,11 +5,14 @@ from fileanalyzer import FileAnalyzer
 class JavaAnalyzer(FileAnalyzer):
     def get_class_count(self, content):
         return len(
-            re.findall("[a-zA-Z ]*class +[a-zA-Z0-9_]+ *\n*\{", content))
+            re.findall("[a-zA-Z ]*class +[a-zA-Z0-9_<>, ]+\n*\{", content))
 
-    # TODO: Accept angle brackets and decline "else if"
     def get_function_count(self, content):
-        return len(
-            re.findall(
-                "[a-zA-Z ]+ +[a-zA-Z0-9_]+ *\n*\([a-zA-Z0-9_,\[\] \n]*\)[a-zA-Z \n]*\{",
-                content))
+        matches = re.findall(
+            "[a-zA-Z <>]+ +[a-zA-Z0-9_]+ *\n*\([a-zA-Z0-9_,\[\]<>\?\. \n]*\)[a-zA-Z \n]*\{",
+            content)
+        matches = [
+            m for m in matches
+            if "if " not in m.strip() and "if(" not in m.strip()
+        ]
+        return len(matches)
