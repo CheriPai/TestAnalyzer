c79e6b16e29dc0c756bfe82d62b9e01a5702c47f
dat <datdo@rocketmail.com>
2016-11-21 16:55:32 -0800

    Fix counter to ignore quoted lines

--- testanalyzer/pythonanalyzer.py
+++ testanalyzer/pythonanalyzer.py
@@ -4,10 +4,11 @@ from fileanalyzer import FileAnalyzer
 
 class PythonAnalyzer(FileAnalyzer):
     def get_class_count(self, content):
-        return len(
-            re.findall("[^\"](class +[a-zA-Z0-9_]+ *\(?[a-zA-Z0-9_, ]*\)? *:)+[^\"]",
-                       content))
+        matches = re.findall("\"*class +[a-zA-Z0-9_]+ *\(?[a-zA-Z0-9_, ]*\)? *:\"*", content)
+        matches = [m for m in matches if m.strip()[0] != "\"" and m.strip()[-1] != "\""]
+        return len(matches)
 
     def get_function_count(self, content):
-        return len(
-            re.findall("[^\"](def +[a-zA-Z0-9_]+ *\([a-zA-Z0-9_, ]*\) *:)+[^\"]", content))
+        matches = re.findall("\"*def +[a-zA-Z0-9_]+ *\([a-zA-Z0-9_, ]*\) *:\"*", content)
+        matches = [m for m in matches if m.strip()[0] != "\"" and m.strip()[-1] != "\""]
+        return len(matches)
